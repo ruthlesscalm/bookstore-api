4b67679b9d2bac09aa4f7b9e39067701861b6e5c
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-23 11:57:57 +0530

    feat: enhance book model validation and update upload form for improved user input

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected successfully")
     } catch (error) {
-        console.error("Datbase connection failed!");
+        console.error("Datbase connection failed!: ", error);
         process.exit(1);
     }
 }
--- models/books.js
+++ models/books.js
@@ -1,16 +1,63 @@
 import mongoose from "mongoose";
 
 const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    description: String,
-    price: Number,
-    rating: Number,
-    genre: String,
-    createdAt: {
-        type: Date,
-        default: Date.now
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 2,
+        maxlength: 150
+    },
+    author: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 2,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        trim: true,
+        maxlength: 2000,
+        default: "No description available"
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 100000,
+        validate: {
+            validator: v => typeof v === "number" && Number.isInteger(v * 100),
+            message: "Price must have at most 2 decimal places (eg: 123.65)"
+        }
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10,
+        validate: {
+            validator: v => typeof v === "number" && Number.isInteger(v * 10),
+            message: "Rating must have at most 1 decimal place (eg: 9.7)"
+        }
+    },
+    genre: {
+        type: String,
+        required: true,
+        enum: [
+            "fiction",
+            "non-fiction",
+            "mystery",
+            "romance",
+            "sci-fi",
+            "fantasy",
+            "thriller",
+            "biography",
+            "self-help",
+            "other"
+        ]
     }
+
 })
 
 const Book = mongoose.model('Book', bookSchema);
